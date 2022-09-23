da050cf99aa06eb56dc91261d34047b90af87ab2
tsatke <tim.satke@gmail.com>
2022-09-23 22:51:12 +0200

    wrap unsafe syscalls in safe impl

--- src/io/mod.rs
+++ src/io/mod.rs
@@ -1,10 +1,25 @@
-use crate::syscall::sys_write;
+use crate::syscall::Syscalls;
 use alloc::string::ToString;
 use kernel_constants::io::fd::FD_SYSOUT;
 
+#[derive(Copy, Clone, Debug, PartialEq, Eq)]
+pub struct FileDescriptor {
+    handle: usize,
+}
+
+impl From<usize> for FileDescriptor {
+    fn from(handle: usize) -> Self {
+        Self { handle }
+    }
+}
+
+impl FileDescriptor {
+    pub fn as_usize(&self) -> usize {
+        self.handle
+    }
+}
+
 pub fn _print(args: core::fmt::Arguments) {
     let s = args.to_string();
-    let s_ptr = s.as_ptr();
-    let s_len = s.len();
-    let _ = sys_write(FD_SYSOUT, s_ptr, s_len);
+    let _ = Syscalls::write(FD_SYSOUT.into(), s);
 }
--- src/syscall/mod.rs
+++ src/syscall/mod.rs
@@ -1,15 +1,66 @@
+use crate::io::FileDescriptor;
 use core::arch::asm;
+use kernel_constants::syscall::error::Errno;
 use kernel_constants::syscall::Syscall;
 
-pub fn sys_write(fd: usize, buf: *const u8, count: usize) -> isize {
+#[cfg(not(target_os = "martim"))]
+compile_error!("Syscalls only implemented for MartimOS");
+
+pub struct Syscalls;
+
+pub type Result<T> = core::result::Result<T, Errno>;
+
+impl Syscalls {
+    /// Write syscall.
+    ///
+    /// Writes `buf` to the given `fd` file descriptor. The amount of bytes
+    /// written is returned upon successful completion.
+    pub fn write<T>(fd: FileDescriptor, buf: T) -> Result<usize>
+    where
+        T: AsRef<[u8]>,
+    {
+        let slice = buf.as_ref();
+        let res = unsafe {
+            // Safety: this is safe because slice is valid here
+            sys_write(fd.as_usize(), slice.as_ptr(), slice.len())
+        };
+        if res < 0 {
+            Err(Errno::try_from(-res).expect("invalid errno"))
+        } else {
+            Ok(res as usize)
+        }
+    }
+
+    /// Read syscall.
+    ///
+    /// Reads from the given `fd` into the given `buf`. The amount of
+    /// bytes read is returned upon successful completion.
+    pub fn read<T>(fd: FileDescriptor, buf: &mut T) -> Result<usize>
+    where
+        T: AsMut<[u8]>,
+    {
+        let slice = buf.as_mut();
+        let res = unsafe {
+            // Safety: this is safe because slice is valid here
+            sys_read(fd.as_usize(), slice.as_mut_ptr(), slice.len())
+        };
+        if res < 0 {
+            Err(Errno::try_from(res).expect("invalid errno"))
+        } else {
+            Ok(res as usize)
+        }
+    }
+}
+
+unsafe fn sys_write(fd: usize, buf: *const u8, count: usize) -> isize {
     unsafe { syscall_3(Syscall::Write, fd, buf as usize, count) }
 }
 
-pub fn sys_read(fd: usize, buf: *mut u8, count: usize) -> isize {
+unsafe fn sys_read(fd: usize, buf: *mut u8, count: usize) -> isize {
     unsafe { syscall_3(Syscall::Read, fd, buf as usize, count) }
 }
 
-pub unsafe fn syscall_3<T>(syscall_number: T, arg1: usize, arg2: usize, arg3: usize) -> isize
+unsafe fn syscall_3<T>(syscall_number: T, arg1: usize, arg2: usize, arg3: usize) -> isize
 where
     T: Into<usize>,
 {
